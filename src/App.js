e540ef74b46710761a89fc50470cf5470b502734
nunnanmanasa682 <nunnanmanasa682@gmail.com>
2022-12-17 10:01:23 +0530

    okey

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react"
-import Message from './redux/Message/message.action'
+import Message from './Message/Message'
+import { Provider } from "react-redux"
+import {store} from './redux/store'
 const App=() => {
    
 return(
   <div>
+    <provider store={store}>
     <h1>
       App Component</h1>
     <hr/>
     <Message/>
+    </provider>
   </div>
 )
 
--- src/redux/Message/message.action.js
+++ src/redux/Message/message.action.js
@@ -1,8 +1,13 @@
-let gmAction =()=>{
-    return{}
+let GM = 'GM'
+let GN = 'GN'
+let gmAction = () => {
+    console.log("second-action")
+    return { type: GM }
+}
+
+let gnAction = () => {
+    return { type: GN }
 
 }
-let gnAction=()=>{
-    return{}
-}
-export {gmAction,gnAction}
+
+export { gmAction, gnAction, GM, GN }
