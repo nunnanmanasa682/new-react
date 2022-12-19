16439d0f5dd4296433d0eb6ba54912b130fa6fba
nunnanmanasa682 <nunnanmanasa682@gmail.com>
2022-12-19 10:41:52 +0530

    okey

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React from "react"
-import Message from './Message/Message'
-import { Provider } from "react-redux"
-import {store} from './redux/store'
+//import Message from './Message/Message'
+//import { Provider } from "react-redux"
+//import {store} from './redux/store'
+import {Link,BroswerRouter as Router, Routes,Route}from 'react-router-dom'
+import Products from './Products/Products'
+import Admin from '/Products/Admin'
+import CreateProduct from './Products/CreateProduct'
+
 const App=() => {
    
 return(
   <div>
-    <provider store={store}>
-    <h1>
-      App Component</h1>
-    <hr/>
-    <Message/>
-    </provider>
+    <router>
+  <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+   <Link to="/">redux thunk example</Link> 
+   <div className="ml-auto">
+    <ul className="navbar-nav">
+      <li className="nav-list"><Link className="nav-link" to="/Products">Products</Link></li>
+      <li className="nav-list"><Link className="nav-link" to ="/create">Create Product</Link></li>
+       <li className="nav-list"><Link className="nav-link"to ="/admin">Admin</Link></li>
+
+       
+    </ul>
+   </div>
+  </nav>
+  <Routes>
+    <Route exact path="/Products" element={<Products/>}/>
+    <Route exact path="/create" element={<CreateProduct/>}/>
+    <Route exact path="/admin" element={<Admin/>} />
+ </Routes>
+  </router>
   </div>
 )
 
